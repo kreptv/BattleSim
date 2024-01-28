84abc548a6ca25324f34b79834a05246e0d675a5
Krieger <73131476+AbsolutelyAri@users.noreply.github.com>
2024-01-28 11:31:03 -0600

    Buffed Maurice

--- BattleSim/Assets/Scripts/BattleManager.cs
+++ BattleSim/Assets/Scripts/BattleManager.cs
@@ -117,14 +117,20 @@ public class BattleManager : MonoBehaviour
             opponentActionText = UseMove(opponent, player, opponentMove, opponentActionText);
             opponentActionTextbox.text = opponentActionText;
             if (playerMove.effects.Contains(MoveEffect.Damage))
+            {
                 MoveAnimationManager.Instance.PlayAnimation(opponentMove.type, opponentMove.animationIndex, true);
+            }
+                
             yield return new WaitForSeconds(baseMoveDelayTime + opponentMove.animTime);
             if (CheckForDeath()) { yield break; }
 
             playerActionText = UseMove(player, opponent, playerMove, playerActionText);
             playerActionTextbox.text = playerActionText;
             if (opponentMove.effects.Contains(MoveEffect.Damage))
+            {
                 MoveAnimationManager.Instance.PlayAnimation(playerMove.type, playerMove.animationIndex, false);
+            }
+                
             yield return new WaitForSeconds(baseMoveDelayTime + playerMove.animTime);
             if (CheckForDeath()) { yield break; }
         }
